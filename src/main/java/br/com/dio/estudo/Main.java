844ed2840b108748c5287093cad6202f486ab10e
Edilson do Nascimento <edilsonjoper@gmail.com>
2023-05-30 16:31:51 -0300

    02 - Criando dominio curso e mentoria e instanciado.

---
+++ src/main/java/br/com/dio/estudo/Main.java
@@ -0,0 +1,31 @@
+package br.com.dio.estudo;
+
+import br.com.dio.estudo.dominio.Curso;
+import br.com.dio.estudo.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de Java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição curso 2");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de java");
+        mentoria.setDescricao("Descricao mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
+}
